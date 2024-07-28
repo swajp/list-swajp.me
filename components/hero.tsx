3516889a6b242db864807c11883e59d08c0cd53d
swajp <stejskalmira@icloud.com>
2024-07-28 10:55:59 +0200

    feat: Add star button

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { buttonVariants } from "./ui/button"
+import { StarIcon } from "lucide-react"
 
 export default function Hero() {
     return (
@@ -8,13 +9,23 @@ export default function Hero() {
                 <h1 className="text-3xl font-medium">
                     It has never been easier to find the right projects or designs by inspiring sucessful people.
                 </h1>
-                <Link
-                    target="_blank"
-                    className={buttonVariants({ variant: "default", className: "!rounded-full w-fit h-8 px-2 " })}
-                    href="https://twitter.com/intent/tweet"
-                >
-                    Tweet about us
-                </Link>
+                <div className="flex gap-2">
+                    <Link
+                        target="_blank"
+                        className={buttonVariants({ variant: "default", className: "!rounded-full w-fit h-8 px-2 " })}
+                        href="https://twitter.com/intent/tweet"
+                    >
+                        Tweet about us
+                    </Link>
+                    <Link
+                        target="_blank"
+                        className={buttonVariants({ variant: "outline", className: "!rounded-full w-fit h-8 px-2 " })}
+                        href="https://github.com/swajp/list-swajp.me"
+                    >
+                        <StarIcon className="w-3 h-3 mr-1" fill="black" />
+                        Give us a star
+                    </Link>
+                </div>
             </div>
         </>
     )
