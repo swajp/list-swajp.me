9abd14b009413bd30275fde076f80b9ac49fa727
swajp <stejskalmira@icloud.com>
2024-07-28 10:47:58 +0200

    feat: Add link to open source code in Changelog page

--- app/(root)/changelog/page.tsx
+++ app/(root)/changelog/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link"
+
 export default function ChangelogPage() {
     return (
         <div className="h-screen pt-12">
             <h1 className="text-2xl font-medium">Changelog</h1>
+            <div className="flex flex-col mt-8">
+                <h2 className="font-medium">v1.2.0</h2>
+                <ul className="text-sm list-disc pl-4 mt-2">
+                    <li>
+                        This projects is now open source, you can find the code{" "}
+                        <Link href={"https://github.com/swajp/list-swajp.me"} className="underline">
+                            here
+                        </Link>{" "}
+                    </li>
+                </ul>
+            </div>
             <div className="flex flex-col mt-8">
                 <h2 className="font-medium">v1.1.2</h2>
                 <ul className="text-sm list-disc pl-4 mt-2">
