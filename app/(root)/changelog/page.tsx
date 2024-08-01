9db5159d794e90036e0bda5989286c0249951197
swajp <stejskalmira@icloud.com>
2024-08-01 12:30:02 +0200

    changelog update

--- app/(root)/changelog/page.tsx
+++ app/(root)/changelog/page.tsx
@@ -5,6 +5,12 @@ export default function ChangelogPage() {
         <div className="h-screen pt-12">
             <h1 className="text-2xl font-medium">Changelog</h1>
             <div className="flex flex-col mt-8">
+                <h2 className="font-medium">v1.6.0</h2>
+                <ul className="text-sm list-disc pl-4 mt-2">
+                    <li>Added profile page where you can see your account details and portfolio requests.</li>
+                </ul>
+            </div>
+            <div className="flex flex-col mt-8">
                 <h2 className="font-medium">v1.5.0</h2>
                 <ul className="text-sm list-disc pl-4 mt-2">
                     <li>Added community so people can share their portfolios</li>
