7ded037a05f2172525a4a0e887dc6dade544df35
swajp <stejskalmira@icloud.com>
2024-08-15 10:27:38 +0200

    improved portfolio submit

--- components/submit-dialog.tsx
+++ components/submit-dialog.tsx
@@ -3,7 +3,7 @@
 import { api } from "@/convex/_generated/api"
 import { useMutation } from "convex/react"
 import { Button } from "./ui/button"
-import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "./ui/dialog"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTrigger } from "./ui/dialog"
 import { Label } from "./ui/label"
 import { Input } from "./ui/input"
 import React from "react"
@@ -12,10 +12,14 @@ import { DialogTitle } from "@radix-ui/react-dialog"
 import { portfolioSubmited } from "@/lib/actions"
 import { toast } from "sonner"
 import { useRouter } from "next/navigation"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function SubmitDialog({ children }: { children: React.ReactNode }) {
     const [name, setName] = React.useState<string | null>(null)
     const [url, setUrl] = React.useState<string | null>(null)
+
+    const [submited, setSubmited] = React.useState(false)
 
     const router = useRouter()
 
@@ -39,7 +43,7 @@ export default function SubmitDialog({ children }: { children: React.ReactNode }
         }
     }
 
-    const handleSubmit = async () => {
+    const handleSubmit = () => {
         if (!name || !url) {
             toast.error("Please fill out all fields")
             return
@@ -47,68 +51,107 @@ export default function SubmitDialog({ children }: { children: React.ReactNode }
 
         if (!user) {
             toast.error("You need to be logged in to submit a portfolio")
+            return
         }
 
         if (!checkUrl(url)) {
             return
         }
-        setIsSubmitting(true)
 
-        await create({
-            name,
-            url
+        const promise = create({ name, url })
+            .then(() => portfolioSubmited(user?.emailAddresses[0].emailAddress!, name, url))
+            .then(() => {
+                setSubmited(true)
+                setName(null)
+                setUrl(null)
+            })
+
+        toast.promise(promise, {
+            loading: "Submitting your portfolio...",
+            success: "Portfolio submitted. Wait for approval",
+            error: "Failed to submit your portfolio"
         })
 
-        await portfolioSubmited(user?.emailAddresses[0].emailAddress!, name, url)
-        setIsSubmitting(false)
-        toast.success("Portfolio submitted. Wait for approval")
+        setIsSubmitting(true)
 
-        router.push("/profile")
-
-        setName(null)
-        setUrl(null)
+        promise.finally(() => {
+            setIsSubmitting(false)
+        })
     }
 
     return (
         <Dialog>
             <DialogTrigger asChild>{children}</DialogTrigger>
-            <DialogContent className="max-w-sm ">
-                <DialogTitle className="text-xl font-medium mx-auto">Submit my portfolio</DialogTitle>
-                <form action={handleSubmit}>
-                    <div className="flex flex-col gap-2 items-center justify-center">
-                        <div className="space-y-4 w-full">
-                            <div>
-                                <Label> Name </Label>
-                                <Input
-                                    value={name || ""}
-                                    onChange={e => setName(e.target.value)}
-                                    disabled={isSubmitting}
-                                    type="text"
-                                    name="name"
-                                    placeholder="swajp"
-                                />
-                            </div>
-
-                            <div>
-                                <Label>URL</Label>
-
-                                <Input
-                                    value={url || ""}
-                                    onChange={e => setUrl(e.target.value)}
-                                    disabled={isSubmitting}
-                                    type="text"
-                                    name="url"
-                                    placeholder="https://swajp.me"
-                                />
-                            </div>
+            <DialogContent className="max-w-sm h-full max-h-96">
+                {!submited ? (
+                    <>
+                        <div className="text-center">
+                            <DialogTitle className="text-xl font-medium mt-2">Submit my portfolio</DialogTitle>
+                            <DialogDescription>
+                                Get more visitors to your portfolio by submitting it to our community. We will review it
+                                and publish it.
+                            </DialogDescription>
                         </div>
-                        <div className="py-4">
-                            <Button disabled={isSubmitting} type="submit" className="w-full rounded-full">
-                                Submit
-                            </Button>
+                        <form action={handleSubmit}>
+                            <div className="flex flex-col gap-2 items-center justify-center">
+                                <div className="space-y-4 w-full">
+                                    <div>
+                                        <Label> Name </Label>
+                                        <Input
+                                            value={name || ""}
+                                            onChange={e => setName(e.target.value)}
+                                            disabled={isSubmitting}
+                                            type="text"
+                                            name="name"
+                                            placeholder="swajp"
+                                        />
+                                    </div>
+
+                                    <div>
+                                        <Label>URL</Label>
+
+                                        <Input
+                                            value={url || ""}
+                                            onChange={e => setUrl(e.target.value)}
+                                            disabled={isSubmitting}
+                                            type="text"
+                                            name="url"
+                                            placeholder="https://swajp.me"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="py-4 w-full">
+                                    <Button disabled={isSubmitting} type="submit" className="w-full rounded-full">
+                                        {isSubmitting ? "Submitting..." : "Submit"}
+                                    </Button>
+                                </div>
+                            </div>
+                        </form>
+                    </>
+                ) : (
+                    <div>
+                        <Image
+                            src={"/submited.gif"}
+                            alt="submited"
+                            width={504}
+                            height={304}
+                            className="rounded-xl border my-4"
+                        />
+                        <div className="text-center">
+                            <DialogTitle className="text-xl font-medium mt-2">Portfolio submitted</DialogTitle>
+                            <DialogDescription>
+                                Your portfolio has been submitted. We will review it and publish it. Meanwhile, you can
+                                give us a{" "}
+                                <Link
+                                    className="text-primary underline"
+                                    href={"https://github.com/swajp/list-swajp.me"}
+                                >
+                                    star
+                                </Link>
+                            </DialogDescription>
                         </div>
                     </div>
-                </form>
+                )}
             </DialogContent>
         </Dialog>
     )
