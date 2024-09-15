a59d158b454670ad79aeab6c03fadddeb8a3299c
swajp <stejskalmira@icloud.com>
2024-09-15 12:54:41 +0200

    code format

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,4 +1,5 @@
 import CommunityList from "@/components/community-list"
+import SubmitActionCard from "@/components/submit-action-card"
 import SubmitDialog from "@/components/submit-dialog"
 import { Button } from "@/components/ui/button"
 import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs"
@@ -7,9 +8,7 @@ export default function CommunityPage() {
     return (
         <div>
             <div className="flex flex-col gap-4 pt-12 pb-8 max-w-lg">
-                <h1 className="text-3xl font-medium">
-                    Show your portfolio to the world and get feedback from the community.
-                </h1>
+                <h1 className="text-3xl font-medium">Show your portfolio to the world and get feedback from the community.</h1>
                 <SignedOut>
                     <SignInButton mode="modal">
                         <Button variant="default" className="!rounded-full w-fit h-8 px-2 ">
