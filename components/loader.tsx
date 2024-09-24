c47270d67ff6ed469189b6a588e351118ba4dea7
swajp <stejskalmira@icloud.com>
2024-09-24 17:56:19 +0200

    loader fix error

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,8 +1,14 @@
 "use client"
 
-import { ping } from "ldrs"
+import { useEffect } from "react"
 
 export default function Loader() {
-    ping.register()
+    useEffect(() => {
+        async function getLoader() {
+            const { ping } = await import("ldrs")
+            ping.register()
+        }
+        getLoader()
+    }, [])
     return <l-ping size="30" speed="1" color="#737373"></l-ping>
 }
