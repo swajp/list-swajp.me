01e6672d5f25b3875c39d8538741243b22f1a7d3
swajp <stejskalmira@icloud.com>
2024-09-24 21:38:50 +0200

    upvoting for portfolios

--- convex/portfolios.ts
+++ convex/portfolios.ts
@@ -19,6 +19,7 @@ export const create = mutation({
             name: args.name,
             url: args.url,
             upvotes: 0,
+            newUpvotes: [],
             published: false,
             views: 0
         })
@@ -92,6 +93,45 @@ export const updateViews = mutation({
 
         await ctx.db.patch(portfolio._id, {
             views: (portfolio.views ?? 0) + 1
+        })
+    }
+})
+
+export const upvote = mutation({
+    args: { portfolioId: v.id("portfolios") },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity()
+
+        if (identity === null) {
+            throw new Error("No logged in user")
+        }
+
+        const portfolio = await ctx.db
+            .query("portfolios")
+            .filter(q => q.eq(q.field("_id"), args.portfolioId))
+            .first()
+
+        if (!portfolio) {
+            throw new Error("Portfolio not found")
+        }
+
+        if (portfolio.owner === identity.subject) {
+            throw new Error("Cannot upvote own portfolio")
+        }
+
+        const upvotes: { userId: string; portfolioId: string }[] = Array.isArray(portfolio.upvotes) ? portfolio.upvotes : []
+        const userUpvoteIndex = upvotes.findIndex(upvote => upvote.userId === identity.subject)
+
+        if (userUpvoteIndex !== -1) {
+            // User has already upvoted, so remove the upvote
+            upvotes.splice(userUpvoteIndex, 1)
+        } else {
+            // User has not upvoted, so add the upvote
+            upvotes.push({ userId: identity.subject, portfolioId: args.portfolioId })
+        }
+
+        await ctx.db.patch(portfolio._id, {
+            newUpvotes: upvotes
         })
     }
 })
