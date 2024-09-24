be54ce4831000ffb01c9f5f81645485963761cb0
swajp <stejskalmira@icloud.com>
2024-09-24 17:36:50 +0200

    increased padding for mobile

--- components/max-width-wrapper.tsx
+++ components/max-width-wrapper.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils"
 
 export default function MaxWidthWrapper({ className, children }: { className?: string; children: React.ReactNode }) {
-    return <div className={cn("h-full mx-auto w-full max-w-screen-2xl px-2.5 md:px-20", className)}>{children}</div>
+    return <div className={cn("h-full mx-auto w-full max-w-screen-2xl px-3 md:px-20", className)}>{children}</div>
 }
