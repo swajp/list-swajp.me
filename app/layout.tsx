0ad0ce154095984fd2e0e4229e441bf7b612c494
swajp <stejskalmira@icloud.com>
2024-09-26 12:22:41 +0200

    replaced vercel analytics by posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { Analytics } from "@vercel/analytics/next"
-import { ClerkProvider } from "@clerk/nextjs"
 import ConvexClientProvider from "@/components/convex-provider-clerk"
 import { Toaster } from "@/components/ui/sonner"
 import { ThemeProvider } from "@/components/theme-provider"
 import NewFeature from "@/components/new-feature"
+import { CSPostHogProvider } from "@/components/posthog-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -25,14 +24,15 @@ export default function RootLayout({
     return (
         <ConvexClientProvider>
             <html lang="en">
-                <body className={inter.className}>
-                    <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="list-swajp-theme" disableTransitionOnChange>
-                        {children}
-                        <NewFeature />
-                        <Analytics />
-                        <Toaster />
-                    </ThemeProvider>
-                </body>
+                <CSPostHogProvider>
+                    <body className={inter.className}>
+                        <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="list-swajp-theme" disableTransitionOnChange>
+                            {children}
+                            <NewFeature />
+                            <Toaster />
+                        </ThemeProvider>
+                    </body>
+                </CSPostHogProvider>
             </html>
         </ConvexClientProvider>
     )
