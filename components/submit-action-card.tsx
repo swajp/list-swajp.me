720cc4de8100fe400f90bd8f9db7ec395be169f1
swajp <stejskalmira@icloud.com>
2024-10-07 22:41:15 +0200

    changed img for submit project

--- components/submit-action-card.tsx
+++ components/submit-action-card.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function SubmitActionCard({ text }: { text: string }) {
     return (
         <div className="relative border rounded-lg overflow-hidden">
-            <Image className="rounded-lg blur-sm " quality={100} src={"/jhey.png"} alt={"More projects"} width={453} height={254} />
+            <Image className="rounded-lg blur-sm " quality={100} src={"/erik.png"} alt={"More projects"} width={453} height={254} />
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <div
                     className={buttonVariants({
