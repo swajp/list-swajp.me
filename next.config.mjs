4d4456b61c04144ac416b4ad79807cb265d047e2
swajp <stejskalmira@icloud.com>
2024-10-08 17:01:05 +0200

    allow clerk images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "img.clerk.com"
+            }
+        ]
+    }
+}
 
-export default nextConfig;
+export default nextConfig
