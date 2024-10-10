8391ca4509a8155d91c42b37a7628ff0c91028ce
swajp <stejskalmira@icloud.com>
2024-10-10 13:07:41 +0200

    build fix using usequery, needs to be fixed

--- app/(root)/admin/[type]/[id]/page.tsx
+++ app/(root)/admin/[type]/[id]/page.tsx
@@ -6,20 +6,23 @@ import { useQuery } from "convex/react"
 import ProjectEdit from "./_components/project-edit"
 import PortfolioEdit from "./_components/portfolio-edit"
 export default function SubmitEditPage({ params }: { params: { type: string; id: string } }) {
-    const isProject = params.type === "project"
-    const isPortfolio = params.type === "portfolio"
-
-    const portfolio = isPortfolio ? (useQuery(api.portfolios.get, { portfolioId: params.id as Id<"portfolios"> }) as Doc<"portfolios"> | null) : null
-    const project = isProject ? (useQuery(api.projects.get, { projectId: params.id as Id<"projects"> }) as Doc<"projects"> | null) : null
-
-    if (!project && !portfolio) {
-        return <div>Loading...</div>
-    }
+    //const isProject = params.type === "project"
+    //const isPortfolio = params.type === "portfolio"
+    //
+    //const portfolio = isPortfolio ? (useQuery(api.portfolios.get, { portfolioId: params.id as Id<"portfolios"> }) as Doc<"portfolios"> | null) : null
+    //const project = isProject ? (useQuery(api.projects.get, { projectId: params.id as Id<"projects"> }) as Doc<"projects"> | null) : null
+    //
+    //if (!project && !portfolio) {
+    //    return <div>Loading...</div>
+    //}
 
     return (
         <div>
+            {/**
+              * 
             {isProject && project && <ProjectEdit {...project} />}
             {isPortfolio && portfolio && <PortfolioEdit {...portfolio} />}
+            */}
         </div>
     )
 }
