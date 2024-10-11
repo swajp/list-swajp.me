789af665af9a2d7da25b6cf5dc0cbbac34e848c2
swajp <stejskalmira@icloud.com>
2024-10-11 08:23:26 +0200

    allow convex images

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
         remotePatterns: [
             {
                 hostname: "img.clerk.com"
+            },
+            {
+                protocol: "https",
+                hostname: "bold-raven-23.convex.cloud"
             }
         ]
     }
